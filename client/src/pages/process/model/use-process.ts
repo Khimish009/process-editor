3e1f9248308d0c2d239bfbf830e454b483c03f46
Сальман Химишев <salman.khimishev01@gmail.com>
2024-12-15 13:06:45 +0300

    feat(*): refactor use-process hook

--- client/src/pages/process/model/use-process.ts
+++ client/src/pages/process/model/use-process.ts
@@ -1,24 +1,6 @@
-import { useEffect, useState } from "react"
-import type { Process } from "./types"
 import { procesApi } from "../api"
+import { useLoad } from "../../../shared/use-load"
 
 export const useProcess = (id: string) => {
-    const [process, setProcess] = useState<Process>()
-    const [isLoading, setIsLoading] = useState(true)
-
-    const fetchProcess = () => {
-        return procesApi.getById(id).then(setProcess)
-    }
-
-    useEffect(() => {
-        setIsLoading(true)
-
-        fetchProcess().finally(() => setIsLoading(false))
-    }, [])
-
-    return {
-        data: process,
-        isLoading,
-        refetch: fetchProcess
-    }
+	return useLoad(() => procesApi.getById(id))
 }
