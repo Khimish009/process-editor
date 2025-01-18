0bcf13d2207cb94fe96a8eb9e312467f27bf7d67
Сальман Химишев <salman.khimishev01@gmail.com>
2025-01-18 16:05:17 +0300

    feat(*): add arrow selection

--- client/src/modules/blocks-flow/facade/arrows.tsx
+++ client/src/modules/blocks-flow/facade/arrows.tsx
@@ -1,19 +1,24 @@
 import { Block, blocksRecord, blocksRelations } from "../domain/block"
+import { useSelected } from "../model/use-selected"
 import { ArrowUI } from "../ui/array"
 import { usePortPositions } from "../view-model/use-ports-positions"
 
 export const Arrows = ({ blocks }: { blocks: Block[] }) => {
     const portPositions = usePortPositions()
-    const record = blocksRecord(blocks) 
+    const record = blocksRecord(blocks)
+    const selected = useSelected(state => state.selectedRelations)
+    const toggleRelation = useSelected(state => state.toggleRelation)
 
-    return (
+    return (  
         <>
             {blocksRelations(blocks).map(relation => (
                 <ArrowUI
                     key={relation.id}  
                     blocksRecord={record} 
                     portPositions={portPositions}
-                    relation={relation} 
+                    relation={relation}
+                    isSelected={selected[relation.id]}
+                    onClick={() => toggleRelation(relation.id)}
                 />
             ))}
         </>
