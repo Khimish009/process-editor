a1999e255799c6b9f90457aed7c40619ea24e228
Сальман Химишев <salman.khimishev01@gmail.com>
2025-01-19 15:51:23 +0300

    feat(useKeysHandlers): add useKeysHandlers

---
+++ client/src/modules/blocks-flow/view-model/use-keys-handlers.ts
@@ -0,0 +1,23 @@
+import { useEffect, useRef } from "react"
+
+export const useKeysHandlers = ({
+    onDelete
+}: {
+    onDelete?: () => void
+}) => {
+    const onDeleteRef = useRef(onDelete) 
+    onDeleteRef.current = onDelete
+
+    const handler = (e: KeyboardEvent) => {
+        if (e.key === "Backspace" || e.key === "Delete") {
+            e.preventDefault()
+            onDeleteRef.current?.()
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener("keydown", handler)
+
+        return () => window.removeEventListener("keydown", handler)
+    }, [])
+}
