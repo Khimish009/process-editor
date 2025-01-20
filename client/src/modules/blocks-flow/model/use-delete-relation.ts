e365b184e61fc6981eca995a182e5e1f4dd261ec
Сальман Химишев <salman.khimishev01@gmail.com>
2025-01-20 17:19:35 +0300

    feat(*): refactor useDeleteRelation

--- client/src/modules/blocks-flow/model/use-delete-relation.ts
+++ client/src/modules/blocks-flow/model/use-delete-relation.ts
@@ -1,38 +1,35 @@
-import { useState } from "react"
+import { create } from "zustand"
 import { blocksFlowApi } from "../api"
 import { Relation, RelationId } from "../domain/block"
 
-export const useDeleteRelation = ({
-    relationsToDelete,
-    onComplete,
-    afterComplete
-}: {
-    relationsToDelete: RelationId[]
-    onComplete: () => Promise<void>
-    afterComplete: () => void
-}) => {
-    const [isLoading, setIsLoading] = useState(false)
- 
-    const deleteRelations = async () => { 
-        setIsLoading(true)
+type Store = {
+    isLoading: boolean
+    deleteRelations: (params: {
+        relationsToDelete: RelationId[]
+        onComplete: () => Promise<void>, 
+        afterComplete: () => void
+    }) => Promise<void>
+    filterDeleted: (
+        relations: Relation[], 
+        relationsToDelete: Record<RelationId, Relation>
+    ) => Relation[]
+}
+
+export const useDeleteRelation = create<Store>((set, get) => ({
+    isLoading: false,
+    deleteRelations: async ({ relationsToDelete, onComplete, afterComplete }) => {
+        set({ isLoading: true })
         await Promise.allSettled(relationsToDelete.map(id => blocksFlowApi.deleteRelation(id)))
         await onComplete()
 
-        setIsLoading(false)
+        set({ isLoading: false })
         afterComplete()
-    }
-
-    const filterDeleted = (relations: Relation[]) => {
-        if (isLoading) {
-            return relations.filter(relation => !relationsToDelete.includes(relation.id))
+    },
+    filterDeleted: (relations, relationsToDelete) => {
+        if (get().isLoading) {
+            return relations.filter(relation => !relationsToDelete[relation.id])
         } 
 
         return relations
     }
-
-    return {
-        isLoading,
-        filterDeleted,
-        deleteRelations
-    }
-}
+}))
