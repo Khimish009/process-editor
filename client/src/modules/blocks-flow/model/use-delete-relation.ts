7b9ad006eeb113d187b053b36bed1c8409af6474
Сальман Химишев <salman.khimishev01@gmail.com>
2025-02-02 14:30:20 +0300

    feat(use-delete-relation): refactor useDeleteRelation hooks

--- client/src/modules/blocks-flow/model/use-delete-relation.ts
+++ client/src/modules/blocks-flow/model/use-delete-relation.ts
@@ -4,30 +4,42 @@ import { Relation, RelationId } from "../domain/block"
 
 type Store = {
     isLoading: boolean
-    deleteRelations: (params: {
+    setIsLoading: (isLoading: boolean) => void
+}
+
+export const useDeleteStore = create<Store>((set) => ({
+    isLoading: false,
+    setIsLoading: (isLoading) => set({ isLoading })
+}))
+
+export const useDeleteRelation = (
+    {
+        relationsToDelete,
+        onComplete,
+        afterComplete
+    }: {
         relationsToDelete: RelationId[]
         onComplete: () => Promise<void>, 
         afterComplete: () => void
-    }) => Promise<void>
-}
+    }
+) => {
+    const setIsLoading = useDeleteStore(state => state.setIsLoading)
 
-export const useDeleteRelation = create<Store>((set) => ({
-    isLoading: false,
-    deleteRelations: async ({ relationsToDelete, onComplete, afterComplete }) => {
-        set({ isLoading: true })
+    return async () => {
+        setIsLoading(true)
         await Promise.allSettled(relationsToDelete.map(id => blocksFlowApi.deleteRelation(id)))
         await onComplete()
 
-        set({ isLoading: false })
+        setIsLoading(false)
         afterComplete()
     }
-}))
+}
 
 export const useFilterDeleted = (
     relations: Relation[], 
     relationsToDelete: Record<RelationId, boolean>
 ) => {
-    const isLoading = useDeleteRelation(state => state.isLoading)
+    const isLoading = useDeleteStore(state => state.isLoading)
 
     return isLoading 
         ? relations.filter(relation => !relationsToDelete[relation.id])
