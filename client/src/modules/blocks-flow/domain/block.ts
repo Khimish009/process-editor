e6d5ad17b4da79075a6c314313763fe9e6547b62
Сальман Химишев <salman.khimishev01@gmail.com>
2025-02-07 18:48:26 +0300

    feat(relationFromPorts): add relationFromPorts

--- client/src/modules/blocks-flow/domain/block.ts
+++ client/src/modules/blocks-flow/domain/block.ts
@@ -1,3 +1,5 @@
+import { getPortId, type Port } from "./port"
+
 export type BlockId = string
 
 export type Block = {
@@ -33,3 +35,21 @@ export const blocksRecord = (blocks: Block[]): Record<BlockId, Block> => {
         {}
     )
 };
+
+export const relationFromPorts = (portA: Port, portB: Port) => {
+    return portA.type === "input" ?
+            {
+                id: `${getPortId(portB)}:${getPortId(portA)}`,
+                inputId: portA.blockId,
+                inputPort: portA.port,
+                outputId: portB.blockId,
+                outputPort: portB.port
+            } :
+            {
+                id: `${getPortId(portA)}:${getPortId(portB)}`,
+                inputId: portB.blockId,
+                inputPort: portB.port,
+                outputId: portA.blockId,
+                outputPort: portA.port
+            }
+}
